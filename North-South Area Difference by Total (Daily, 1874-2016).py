d34d55aac3faef8e3b6cf8d1f48f7b29030814a6
ratuldas6 <53458166+ratuldas6@users.noreply.github.com>
2020-06-28 10:02:25 +0530

    diffbytotal

---
+++ North-South Area Difference by Total (Daily, 1874-2016).py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
+from matplotlib import dates as mpl_dates
+
+plt.style.use('seaborn')
+
+data = pd.read_csv('area_1874-2016.csv')
+
+data['Date'] = pd.to_datetime(data['Date'])
+data.sort_values('Date', inplace=True)
+
+sunspot_date = data['Date']
+sunspot_areadiff = data['Difference']
+sunspot_totalarea = data['Total']
+AS = []
+for i in range(len(sunspot_totalarea)):
+    if sunspot_totalarea[i] == 0:
+        AS.append(0)
+    else:
+        AS.append(sunspot_areadiff[i]/sunspot_totalarea[i])
+
+plt.plot_date(sunspot_date, AS, markersize = 1, c=(0,0,0))
+
+plt.gcf().autofmt_xdate()
+
+scale_factor = 2
+ymin, ymax = plt.ylim()
+plt.ylim(ymin * scale_factor, ymax * scale_factor)
+plt.title('Daily Hemispherical Sunspot Area Difference by Total (1874-2016)')
+plt.xlabel('Time')
+plt.ylabel('AS')
+plt.grid()
+
+plt.show()
